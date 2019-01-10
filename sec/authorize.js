82b85e7168de9ee8a97f49b72ee25f6f9ec183e3
delProfundo <bruno@hypermedia.tech>
2019-01-11 00:56:28 +1000

    update to base secuirty module and package update

--- sec/authorize.js
+++ sec/authorize.js
@@ -1,164 +1,140 @@
-/********************************************
- * BASIC SERVICE CUSTOM AUTHORIZOR
- * varation of two more basic tools published by aws
- * 23 Dec 2017
+/**
+ * CUSTOM AUTHORIZER
+ * 11 Jan 2019
  * delprofundo (@brunowatt)
  * bruno@hypermedia.tech
- ********************************************/
-const COGNITO_USER_POOL_ID = process.env.COGNITO_USER_POOL_ID;
-const COGNITO_REGION = process.env.COGNITO_REGION;
-const logger = require( "log-winston-aws-level" );
-const request = require("request");
-const jwkToPem = require("jwk-to-pem");
-const jwt = require( "jsonwebtoken" );
-const policyGenerator = require( "./policyGenerator.js" );
-const permissionsMatrix = require( "./servicePermissionsMatrix" );
-
-const tokenIssuer = "https://cognito-idp." + COGNITO_REGION + ".amazonaws.com/" + COGNITO_USER_POOL_ID;
-let pems; // this will hold the key required to decode the JTW
-
-/**
- * basic initial auth routine. simply decodes the token
- * and confirms the users role exists in the types array.
- * @param event
- * @param context
- * @param callback
- * @returns {Promise<void>}
+ * @module sec/authorize
  */
-exports.handler = function( event, context, callback ) {
-  if (!pems) {
-    try {
-      const tokenPath = `${ tokenIssuer }/.well-known/jwks.json`;
-      request.get({
-        url: tokenPath,
-        json: true
-      }, function ( err, response, body ) {
-        if ( !err && response.statusCode === 200 ) {
-          pems = {};
-          let keys = body[ "keys" ];
-          for (let i = 0; i < keys.length; i++) {
-            //Convert each key to PEM
-            let key_id = keys[ i ].kid;
-            let modulus = keys[ i ].n;
-            let exponent = keys[ i ].e;
-            let key_type = keys[ i ].kty;
-            let jwk = { kty: key_type, n: modulus, e: exponent };
-            pems[ key_id ] = jwkToPem( jwk );
+const logger = require("log-winston-aws-level");
+const moment = require( "moment" );
+import { unstring } from "../lib/awsHelpers/general.helper.library";
+const jose  = require( "node-jose" );
+const permissionsMatrix = require( "./permissionsMatrix" );
+const JWA_KEY = process.env.JWA_KEY;
+const MAX_TOKEN_EXPIRY = process.env.MAX_TOKEN_EXPIRY;
+
+export const handler = ( event, context, callback ) => {
+  console.log(moment().add( MAX_TOKEN_EXPIRY, "minutes" ).unix());
+  console.log( MAX_TOKEN_EXPIRY );
+  let localKeystore = jose.JWK.createKeyStore();
+  let localClientRecord;
+  // 1. use the X-Auth-Class-Id header to look up client record
+  localKeystore.add( JWA_KEY )
+    .then(() => {
+      decryptClientJWE(event.headers["Authorization"], localKeystore)
+        .then(decryptedPayload => {
+          console.log("DECRYPTED : ", decryptedPayload);
+          localClientRecord = unstring( decryptedPayload );
+
+          if( hasTokenExpired( localClientRecord.expiry ) === true ) {
+            logger.info( "token has expired, auth failed" );
+            callback( context.fail( "Unauthorized" ));
           }
-          //Now continue with validating the token
-          validateToken( pems, event, context )
-            .then( policy => {
-              logger.info( "returning this policy : ", policy );
-              callback( null, policy );
-            })
-            .catch( err => {
-              logger.error( "error creating policy : ", err.message );
-              callback( context.fail( "Unauthorized" ));
-            });
-        } else {
-          //Unable to download JWKs, fail the call
-          logger.error( "failing cause its an error", err );
-          callback( context.fail( "error" ));
-        }
-      });
-    } catch ( err ) {
-      logger.error( "error in create pem path : ", err );
-      callback( context.fail( "Unauthorized" ));
-    }
-  } else {
-    // validate
-    try {
-      console.log( "in else try" );
-      //Now continue with validating the token
-      validateToken( pems, event, context )
-        .then( policy => {
-          console.log( "returning this policy : ", policy );
-          callback( null, policy );
+
+          let permissionCheckParams = {
+            path: extractBasePath(event.path),
+            resource: event.resource,
+            method: event.httpMethod,
+            memberRole: localClientRecord.role
+          };
+          console.log("permCheckParams: ", permissionCheckParams);
+
+          return permissionsMatrix.validateAccess(permissionCheckParams);
         })
-        .catch( err => {
-          logger.error( "error creating policy : ", err.message );
+        .then(resultEffect => {
+          console.log("success from perm validate access");
+          return buildIamPolicy({
+            memberId: localClientRecord.memberId,
+            effect: resultEffect.effect,
+            resource: event.methodArn,
+            context: localClientRecord
+          });
+        })
+        .then(policyDocument => {
+          callback(null, policyDocument);
+        })
+        .catch(err => {
+          console.log( "err in authorizer : ", err  );
           callback( context.fail( "Unauthorized" ));
         });
-    } catch ( err ) {
-      logger.error( "error in existing pem path : ", err );
-      callback( context.fail( "Unauthorized" ));
-    }
-  }
-}; // end authorisation handler
+    });
+};
 
 /**
- * takes the extracted pems and the event stuff
- * to validate and return the policy
- * @param pems
- * @param event
+ * function returns bool stating if the unix time passed in is either
+ * in the past or beyond what is acceptable for age.
+ * @param tokenExpiryUnixTime
+ * @returns {boolean}
+ */
+function hasTokenExpired( tokenExpiryUnixTime ) {
+  logger.info( "passed in epoch time", tokenExpiryUnixTime );
+  if( moment().unix() >= tokenExpiryUnixTime ) {
+    logger.info( "now is greater than the token expiry" );
+    return true;
+  } else {
+    console.log( tokenExpiryUnixTime >= moment().add( MAX_TOKEN_EXPIRY, "minutes" ).unix());
+    return tokenExpiryUnixTime >= moment().add( MAX_TOKEN_EXPIRY, "minutes" ).unix();
+  }
+} // end hasTokenExpired
+
+function decryptClientJWE( jwe, keystore ) {
+  return new Promise(( resolve, reject ) => {
+    jose.JWE.createDecrypt( keystore )
+      .decrypt( jwe )
+      .then( decryptedJWE => {
+        console.log( "decrypt success" );
+        resolve( JSON.parse(decryptedJWE.payload.toString()));
+      })
+      .catch( err => {
+        console.log( "failed decrypt", err );
+        reject( err );
+      });
+  });
+} // end decryptClientJWE
+
+/**
+ * Function that creates an API Gateway policy document from validated input
+ * @param memberId
+ * @param effect
+ * @param resource
  * @param context
  * @returns {Promise<any>}
  */
-function validateToken( pems, event, context ) {
+function buildIamPolicy({ memberId, effect, resource, context }) {
   return new Promise(( resolve, reject ) => {
-    try {
-      const incomingToken = event.headers.Authorization;
-      const decodedToken = jwt.decode( incomingToken, { complete: true });
-      logger.info( "decoded token : ", JSON.stringify( decodedToken ));
-      //TODO: THINK ABOUT THIS
-      const tokenContext = decodedToken.tokenContext;
-      if (!decodedToken) {
-        logger.error("Not a valid JWT token");
-        context.fail("Unauthorized");
-        reject();
-      }
-      //Fail if token is not from your UserPool
-      if (decodedToken.payload.iss !== tokenIssuer) {
-        logger.error("invalid issuer");
-        context.fail("Unauthorized");
-        reject();
-      }
-      //Reject the jwt if it"s not an "Access Token"
-      if (decodedToken.payload.token_use !== "access") {
-        logger.error("Not an access token");
-        context.fail("Unauthorized");
-        reject();
-      }
-      //Get the kid from the token and retrieve corresponding PEM
-      let kid = decodedToken.header.kid;
-      let pem = pems[kid];
-      if (!pem) {
-        logger.error("Invalid access token");
-        context.fail("Unauthorized");
-        reject();
-      }
-      //now verify that its from where its supposed to be and then we can think about assembly
-      jwt.verify( incomingToken, pem, { issuer: tokenIssuer }, ( err, payload ) => {
-        if( err ) {
-          logger.error( "token failed verification : ", err );
-          context.fail( "Unauthorized" );
-        } else {
-          // put together parameters to check permissions with
-          let permissionsCheckParams = {
-            path: extractBasePath( event.path ),
-            resource: event.resource,
-            method: event.httpMethod,
-            userType: decodedToken.payload[ "cognito:groups" ][ 0 ].toUpperCase() // TODO : check that this really works, that the first in the array has lowest priority always
-          };
-          //determine permission value bool
-          const isAllowed = permissionsMatrix( permissionsCheckParams );
-
-          logger.info( "outcome of some on role arr : ", isAllowed );
-          const effect = isAllowed ? "Allow" : "Deny";
-          const userId = decodedToken.payload.sub;
-          const authorizerContext = {}; //solve context
-          const policyDocument = policyGenerator( userId, effect, event.methodArn, authorizerContext );
-
-          logger.info( "returning IAM policy document : ", policyDocument );
-          resolve( policyDocument );
-        }
-      });
-    } catch ( err ) {
-      logger.error( "err from IAM policy generation", err );
-      reject( err );
+    //test all input is valid and reject if not
+    if(typeof memberId === "undefined" || memberId === null ) {
+      reject( new Error( "memberId required to identify user or client" ));
     }
+    if(typeof effect === "undefined" || effect === null ) {
+      reject( new Error( "invalid effect field" ));
+    }
+    if(typeof resource === "undefined" || resource === null ) {
+      reject( new Error( "invalid resource field" ));
+    }
+    //assemble IAM policy document to return
+    const policy = {
+      principalId: memberId,
+      policyDocument: {
+        Version: "2012-10-17",
+        Statement: [
+          {
+            Effect: effect,
+            Action: [
+              "execute-api:Invoke"
+            ],
+            Resource: [
+              resource
+            ]
+          }
+        ]
+      },
+      context
+    };
+    console.log( "generated policy document: ", JSON.stringify( policy ));
+    resolve( policy );
   });
-}
+} // end buildIAMPolicy
 
 /**
  * takes a path from an APIG event record and
@@ -166,8 +142,9 @@ function validateToken( pems, event, context ) {
  * @param path
  * @returns {string}
  */
+
 function extractBasePath( path ) {
   //break up the path at the back slashes
-  const pathSegments = path.split( "/" ).slice( 1 );
-  return `/${ pathSegments[ 0 ]}`;
-}
+  const pathSegments = path.split("/").slice(1);
+  return `/${pathSegments[ 0 ]}`;
+} // extractBasePath
---
+++ sec/permissionsMatrix.js
@@ -0,0 +1,139 @@
+/********************************************
+ * AUTH HARNESS PERMISSIONS MATRIX
+ * simple matrix mapping permissions to functions in
+ * this service.
+ * 22 March 2018
+ * delProfundo (@brunowatt)
+ * bruno@hypermedia.tech
+ ********************************************/
+const logger = require( "log-winston-aws-level" );
+const SERVICE_BASE_PATH = process.env.SERVICE_BASE_PATH;
+
+const CLIENT_TYPES = {
+  GATEWAY: "GATEWAY", // a partner service that is directly integrated
+  CLIENT: "CLIENT", // a user authenticed interface, cognito/s3/react for instance
+  NODE: "NODE", // an internal component of our system
+  LEAF: "LEAF", // semi-intelligent data source only (ie: iot device)
+  ADMIN: "ADMIN",
+  AUTH_ADMIN: "AUTH_ADMIN",
+  SYSTEM: "SYSTEM"
+};
+
+const MEMBER_ROLES = {
+  MEMBER: "MEMBER",
+  BRAND_ADMIN: "BRAND_ADMIN",
+  PLATFORM_ADMIN: "PLATFORM_ADMIN",
+  SYSTEM: "SYSTEM"
+};
+
+const METHODS = {
+  GET: "GET",
+  POST: "POST",
+  PUT: "PUT",
+  DELETE: "DELETE"
+};
+
+/**
+ * matrix interface, returns
+ * @param path
+ * @param resource
+ * @param method
+ * @param memberRole
+ * @param clientType
+ * @returns {Promise<any>}
+ */
+export async function validateAccess({ path, resource, method, memberRole, clientType }) {
+  return new Promise(( resolve, reject ) => {
+    // this is to switch between users/clients
+    let effectiveEntity;
+
+    if( memberRole ) {
+      effectiveEntity = memberRole;
+    } else if ( clientType ) {
+      effectiveEntity = clientType;
+    } else {
+      console.log( "neither client or user type present, deny access" );
+      return resolve({ effect: "deny" });
+    }
+    let pathObj = permissionsMatrix.find( pathItem => {
+      return pathItem.path === path;
+    });
+    let resourceObj = pathObj.resources.find( resourceItem => {
+      return resourceItem.resource === resource;
+    });
+    if( typeof resourceObj === "undefined" || resourceObj === null ) {
+      return resolve({ effect: "deny" });
+    }
+    let methodObj = resourceObj.methods.find( methodItem => {
+      return methodItem.method === method;
+    });
+    if( typeof methodObj === "undefined" || methodObj === null ) {
+      return resolve({ effect: "deny" });
+    }
+    let deniedUser = methodObj.deny.find(denyItem => {
+      return denyItem === effectiveEntity;
+    });
+    if (deniedUser === effectiveEntity) {
+      return resolve({effect: "deny"});
+    }
+    let permittedUser = methodObj.allow.find( accessItem => {
+      return accessItem === effectiveEntity;
+    });
+    if( typeof permittedUser === "undefined" || permittedUser === null ) {
+      console.log("failed as user type not in allow list");
+      resolve({ effect: "deny" });
+    } else {
+      resolve({ effect: "allow" });
+    }
+  });
+}
+
+/**
+ * This array stores an object for each API path with nested entries allowing simple lookup of permissions
+ */
+const permissionsMatrix = [
+  {
+    path: `/${SERVICE_BASE_PATH}`,
+    resources: [
+      {
+        resource: "/",
+        methods: [
+          {
+            method: METHODS.POST,
+            allow: [
+              MEMBER_ROLES.MEMBER
+            ],
+            deny: [
+            ]
+          }
+        ]
+      },
+      {
+        resource: "/{id}/follow",
+        methods: [
+          {
+            method: METHODS.PUT,
+            allow: [
+              MEMBER_ROLES.MEMBER
+            ],
+            deny: [
+            ]
+          }
+        ]
+      },
+      {
+        resource: "/{id}/unfollow",
+        methods: [
+          {
+            method: METHODS.PUT,
+            allow: [
+              MEMBER_ROLES.MEMBER
+            ],
+            deny: [
+            ]
+          }
+        ]
+      }
+    ]
+  }
+];
