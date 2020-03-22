1345d20a85b52f5edc1f582d8c841c9185608767
delProfundo <bruno@hypermedia.tech>
2020-03-22 23:34:34 +1000

    update to RESifier

--- src/lib/awsHelpers/RESifier.representor.library.js
+++ src/lib/awsHelpers/RESifier.representor.library.js
@@ -8,17 +8,24 @@
  * If no status present standard 200 returned
  * @param candidateObject
  * @param statusCode
- * @returns {{body: string, statusCode: number, headers: {"Access-Control-Allow-Origin": string, "Access-Control-Allow-Credentials": boolean}}}
+ * @param extraHeaders
+ * @returns {{headers: {"Access-Control-Allow-Origin": string, "Access-Control-Allow-Credentials": boolean}, body: string, statusCode: number}}
+ * @constructor
  */
-export const RESifySuccess = ( candidateObject, statusCode = 200 ) => {
-  return {
+export const RESifySuccess = ( candidateObject, statusCode = 200, extraHeaders ) => {
+  const payload = {
     body: JSON.stringify( candidateObject ),
     statusCode: statusCode,
     headers: {
       "Access-Control-Allow-Origin" : "*", // Required for CORS support to work
-      "Access-Control-Allow-Credentials" : true // Required for cookies, authorization headers with HTTPS
+      "Access-Control-Allow-Credentials" : true, // Required for cookies, authorization headers with HTTPS
+      ...extraHeaders
     }
   };
+  if(!candidateObject) {
+    delete payload.body
+  }
+  return payload;
 }; // end RESifySuccess
 
 /**
